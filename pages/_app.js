a0987fe99ac4ebbcc2bb1be6dd86c3bed5fec04e
tony-ojeda <tojeda96@gmail.com>
2022-08-24 18:33:18 -0500

    background

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>@2021 Tony Ojeda</p>
+      </footer>
+    </>
+  )
 }
 
 export default MyApp
